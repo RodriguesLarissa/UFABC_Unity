9cc52ea7e8d2d18799597057cb0ce088358ab5f8
U-NGM-01\guime <gmelo.dsilva@gmail.com>
2021-09-28 21:52:42 -0300

    Modificações 2 - Adição de sons e tela de créditos

--- Forca/Assets/Scripts/GameManager.cs
+++ Forca/Assets/Scripts/GameManager.cs
@@ -13,12 +13,14 @@ public class GameManager : MonoBehaviour
     public GameObject centro; // objeto de texto que indica o centro da tela
 
     private string palavraOculta = ""; // palavra a ser descoberta
-
-    //private string[] palavrasOculta = new string[] {"teste","bola","palavra"}; //array de palavras ocultas
-
     private int tamanhoPalavraOculta; // tamanho da palavra oculta
     char[] letrasOcultas; // letras da palavra oculta
     bool[] letrasDescobertas; // indicador de quais letras foram descobertas
+
+
+    // Variaveis de sons do jogo
+    public static AudioClip audioBeep, audioError;
+    static AudioSource audioSrc;
 
     // Start is called before the first frame update
     void Start(){
@@ -31,13 +33,19 @@ public class GameManager : MonoBehaviour
         maxNumTentativas = 10;
         UpdateNumTentativas();
         UpdateScore();
+
+        //Inicializa sons do jogo
+        audioBeep = Resources.Load<AudioClip>("beep"); //som de letra certa
+        audioError = Resources.Load<AudioClip>("error-01"); // som de letra errada
+        audioSrc = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
-    void Update(){
-        CheckTeclado();
+    void Update(){  
+        CheckTeclado();         
     }
 
+    //Inicializa letras
     void InitLetras(){
         int numLetras = tamanhoPalavraOculta;
         
@@ -49,10 +57,8 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    //Inicializa jogo
     void InitGame(){
-        //palavraOculta = "Elefante"; // definição da palavra a ser descoberta
-        //int numeroAleatorio = Random.Range(0, palavrasOculta.Length); // sortea uma palavra aleatoria
-        //palavraOculta = palavrasOculta[numeroAleatorio];
         
         palavraOculta = PegaUmaPalavraDoArquivo();
         tamanhoPalavraOculta = palavraOculta.Length; // determina-se o numero de letras da palavra oculta
@@ -62,7 +68,8 @@ public class GameManager : MonoBehaviour
         letrasOcultas = palavraOculta.ToCharArray(); // copia-se a palavra no array de letras
     }
 
-    void CheckTeclado(){
+    // Verifica teclado
+    void CheckTeclado(){  
         if(Input.anyKeyDown){
             char letraTeclado = Input.inputString.ToCharArray()[0];
             int letraTecladoInt = System.Convert.ToInt32(letraTeclado);
@@ -73,9 +80,10 @@ public class GameManager : MonoBehaviour
                 UpdateNumTentativas();
 
                 if(numTentativas > maxNumTentativas){
-                    SceneManager.LoadScene("Lab1_forca");
+                    SceneManager.LoadScene("Lab1_forca");               
                 }
 
+                bool encontrouLetra = false;
                 for (int i = 0; i < tamanhoPalavraOculta; i++){
                     if(!letrasDescobertas[i]){
                         letraTeclado = System.Char.ToUpper(letraTeclado);
@@ -86,12 +94,18 @@ public class GameManager : MonoBehaviour
                             score++;
                             PlayerPrefs.SetInt("score", score);
                             UpdateScore();
-                            verificaSePalavraDescoberta();
-                        }
+                            encontrouLetra = true;
+                            audioSrc.PlayOneShot(audioBeep);
+                            VerificaSePalavraDescoberta();
+                        }                      
                     }
                 }
-            }
-        }
+                //Caso a teclada digitada não corresponda a nenhuma letra
+                if(!encontrouLetra){
+                    audioSrc.PlayOneShot(audioError); 
+                }
+            }           
+        }  
     }
 
     void UpdateNumTentativas(){
@@ -102,7 +116,7 @@ public class GameManager : MonoBehaviour
         GameObject.Find("scoreUI").GetComponent<Text>().text = "Score: " + score;
     }
 
-    void verificaSePalavraDescoberta(){
+    void VerificaSePalavraDescoberta(){
         bool condicao = true;
         for (int i = 0; i < tamanhoPalavraOculta; i++)
         {
--- Forca/Assets/Scripts/ManageButton.cs
+++ Forca/Assets/Scripts/ManageButton.cs
@@ -10,12 +10,13 @@ public class ManageButton : MonoBehaviour
         PlayerPrefs.SetInt("score", 0);
     }
 
-    // Update is called once per frame
-    void Update(){
-        
-    }
-
     public void StartMundoGame(){
         SceneManager.LoadScene("Lab1");
+    }
+
+    //Exibe a tela de créditos e finaliza o jogo
+    public void FinalizarJogo(){
+        SceneManager.LoadScene("Creditos");
+        Application.Quit(); //Finalizar aplicação
     }
 }
