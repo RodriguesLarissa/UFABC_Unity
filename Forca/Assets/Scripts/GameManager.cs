a1473c763c5e7ec32f53977cebe0f7123c753a29
RodriguesLarissa <rodrigues_larissa@outlook.com>
2021-09-30 01:55:18 -0300

    Comentarios no codigo

--- Forca/Assets/Scripts/GameManager.cs
+++ Forca/Assets/Scripts/GameManager.cs
@@ -8,7 +8,7 @@ public class GameManager : MonoBehaviour
 {
     private int numTentativas; // armazena as tentativas válidas da rodada
     private int maxNumTentativas; // numero maximo de tentativas para Forca ou Salvação
-    int score = 0;
+    int score = 0; // a pontuação do jogo inicia com 0 pontuações
     public GameObject letra; // prefab da letra no Game
     public GameObject centro; // objeto de texto que indica o centro da tela
 
@@ -24,13 +24,13 @@ public class GameManager : MonoBehaviour
 
     // Start is called before the first frame update
     void Start(){
-        centro = GameObject.Find("centroDaTela");
+        centro = GameObject.Find("centroDaTela"); // localiza o gameobject centroDaTela
 
-        InitGame();
+        InitGame(); 
         InitLetras();
 
-        numTentativas = 0;
-        maxNumTentativas = 10;
+        numTentativas = 0; // a quantidade de tentativas inicializa como zero
+        maxNumTentativas = 10; // determinacao da quantidade maxima de tentativas
         UpdateNumTentativas();
         UpdateScore();
 
@@ -50,10 +50,10 @@ public class GameManager : MonoBehaviour
         int numLetras = tamanhoPalavraOculta;
         
         for(int i=0; i<numLetras; i++){
-            Vector3 novaPosicao = new Vector3(centro.transform.position.x + ((i-numLetras/2.0f)*80), centro.transform.position.y, centro.transform.position.z);
-            GameObject l = (GameObject) Instantiate(letra, novaPosicao, Quaternion.identity);
-            l.name = "letra" + (i+1);
-            l.transform.SetParent(GameObject.Find("Canvas").transform);
+            Vector3 novaPosicao = new Vector3(centro.transform.position.x + ((i-numLetras/2.0f)*80), centro.transform.position.y, centro.transform.position.z); // posicao de cada letra
+            GameObject l = (GameObject) Instantiate(letra, novaPosicao, Quaternion.identity); // instanciação do game object
+            l.name = "letra" + (i+1); // determinacao do nome do game object
+            l.transform.SetParent(GameObject.Find("Canvas").transform); // determinacao do pai do game object
         }
     }
 
@@ -66,6 +66,7 @@ public class GameManager : MonoBehaviour
         letrasOcultas = new char[tamanhoPalavraOculta]; // instancia-se o array char das letras da palavra 
         letrasDescobertas = new bool[tamanhoPalavraOculta]; // instancia-se o array bool dos indicador de letras certas
         letrasOcultas = palavraOculta.ToCharArray(); // copia-se a palavra no array de letras
+
     }
 
     // Verifica teclado
@@ -76,25 +77,25 @@ public class GameManager : MonoBehaviour
 
             if(letraTecladoInt >= 97 && letraTecladoInt <= 122){
 
-                numTentativas++;
+                numTentativas++; // aumenta o numero de vezes que o jogador tentou
                 UpdateNumTentativas();
 
                 if(numTentativas > maxNumTentativas){
-                    SceneManager.LoadScene("Lab1_forca");               
+                    SceneManager.LoadScene("Lab1_forca"); // ao ultrapassar o numero de tentativas, sera movido para a cena de derrota        
                 }
 
                 bool encontrouLetra = false;
                 for (int i = 0; i < tamanhoPalavraOculta; i++){
-                    if(!letrasDescobertas[i]){
+                    if(!letrasDescobertas[i]){ // entrará na função apenas se não for uma letra inserida anteriormente
                         letraTeclado = System.Char.ToUpper(letraTeclado);
-                        if(letrasOcultas[i] == letraTeclado){
-                            letrasDescobertas[i] = true;
-                            GameObject.Find("letra" + (i+1)).GetComponent<Text>().text = letraTeclado.ToString();
-                            score = PlayerPrefs.GetInt("score");
-                            score++;
-                            PlayerPrefs.SetInt("score", score);
+                        if(letrasOcultas[i] == letraTeclado){ // se o jogador pressionou uma letra da palavra oculta, entrará na formula
+                            letrasDescobertas[i] = true; 
+                            GameObject.Find("letra" + (i+1)).GetComponent<Text>().text = letraTeclado.ToString(); // substitui a interrogação pela letra
+                            score = PlayerPrefs.GetInt("score"); // pega o valor do score
+                            score++; // incremente o valor do score
+                            PlayerPrefs.SetInt("score", score); // insere o novo valor do score
                             UpdateScore();
-                            encontrouLetra = true;
+                            encontrouLetra = true; // informa que a letra foi encontrada
                             audioSrc.PlayOneShot(audioBeep);
                             VerificaSePalavraDescoberta();
                         }                      
@@ -102,18 +103,18 @@ public class GameManager : MonoBehaviour
                 }
                 //Caso a teclada digitada não corresponda a nenhuma letra
                 if(!encontrouLetra){
-                    audioSrc.PlayOneShot(audioError); 
+                    audioSrc.PlayOneShot(audioError); // se a letra nao foi encontrada na palavra oculta, tocará um audio para o erro do jogador
                 }
             }           
         }  
     }
 
     void UpdateNumTentativas(){
-        GameObject.Find("numTentativas").GetComponent<Text>().text = numTentativas + " | " + maxNumTentativas;
+        GameObject.Find("numTentativas").GetComponent<Text>().text = numTentativas + " | " + maxNumTentativas; // texto na interface para informar o jogador quantas tentativas restantes
     }
 
     void UpdateScore(){
-        GameObject.Find("scoreUI").GetComponent<Text>().text = "Score: " + score;
+        GameObject.Find("scoreUI").GetComponent<Text>().text = "Score: " + score; // texto na interface para informar o jogador sobre a pontuação
     }
 
     void VerificaSePalavraDescoberta(){
@@ -124,15 +125,15 @@ public class GameManager : MonoBehaviour
         }
         if(condicao){
             PlayerPrefs.SetString("ultimaPalavraOculta", palavraOculta);
-            SceneManager.LoadScene("Lab1_salvo");
+            SceneManager.LoadScene("Lab1_salvo"); // cena que mostra a vitoria do jogador
         }
     }
 
     string PegaUmaPalavraDoArquivo(){
-        TextAsset t1 = (TextAsset)Resources.Load("palavras1", typeof(TextAsset));
-        string s = t1.text;
-        string[] palavras = s.Split(' ');
-        int palavraAleatoria = Random.Range(0, palavras.Length + 1);
-        return (palavras[palavraAleatoria]);
+        TextAsset t1 = (TextAsset)Resources.Load("palavras1", typeof(TextAsset)); // arquivo com todas as palavras que serão usadas
+        string s = t1.text; 
+        string[] palavras = s.Split(' '); // coloca todas as palavras em um array
+        int palavraAleatoria = Random.Range(0, palavras.Length + 1); // pega uma palavra aleatoria para o jogo
+        return (palavras[palavraAleatoria]); // retorna a palavra aleatoria
     }
 }
--- Forca/Assets/Scripts/ManageButton.cs
+++ Forca/Assets/Scripts/ManageButton.cs
@@ -7,16 +7,16 @@ public class ManageButton : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start(){
-        PlayerPrefs.SetInt("score", 0);
+        PlayerPrefs.SetInt("score", 0); // Score inicializa com zero
     }
 
     public void StartMundoGame(){
-        SceneManager.LoadScene("Lab1");
+        SceneManager.LoadScene("Lab1"); // Ao clicar no botão 'Play', irá para a cena do jogo
     }
 
     //Exibe a tela de créditos e finaliza o jogo
     public void FinalizarJogo(){
-        SceneManager.LoadScene("Creditos");
+        SceneManager.LoadScene("Creditos"); // Ao clicar em 'Exit', encerra o jogo
         Application.Quit(); //Finalizar aplicação
     }
 }
