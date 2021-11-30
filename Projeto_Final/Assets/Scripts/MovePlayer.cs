94d1fcd2c8ee0655ca9310d7ea095622d8073e9f
RodriguesLarissa <rodrigues_larissa@outlook.com>
2021-11-29 22:49:20 -0300

    Código para movimentar o player

--- Projeto_Final/Assets/Scripts/MovePlayer.cs
+++ Projeto_Final/Assets/Scripts/MovePlayer.cs
@@ -6,20 +6,67 @@ public class MovePlayer : MonoBehaviour
 {
     public float velocityMove = 3.0f; // equivale ao momento (impulso) a ser dado ao player
     Vector2 Movement = new Vector2(); // detectar movimento pelo teclado
+    Animator animator; // guarda a componente do Controlador de Animação
+    string animationState = "animationState"; // guarda o nome do parametro de Animação
     Rigidbody2D rb2D; // guarda a componente CorpoRigido do Player
+
+    /*
+        Enumera o número da condição de cada movimento
+    */
+    enum EstadosCaractere{
+        andaLeste = 1,
+        andaOeste = 2,
+        andaNorte = 3,
+        andaSul = 4,
+        idle = 5
+    }
 
     // Start is called before the first frame update
     void Start()
     {
+        animator = GetComponent<Animator>(); // recebe o componente animator
         rb2D = GetComponent<Rigidbody2D>(); // recebe o componente RigidBody do Player
     }
 
     // Update is called once per frame
     void Update()
     {
+        StateUpdate();
+    }
+
+    private void FixedUpdate() {
+        MoveCaractere();
+    }
+
+    /*  
+        Função que movimenta o player.
+    */
+    private void MoveCaractere(){
         Movement.x = Input.GetAxisRaw("Horizontal");
         Movement.y = Input.GetAxisRaw("Vertical");
         Movement.Normalize();
         rb2D.velocity = Movement * velocityMove;
+    }
+
+
+    /*
+        Função que atualiza o estado do player e o atualiza a sprite conforme o movimento.
+    */
+    private void StateUpdate(){
+        if(Movement.x > 0){
+            animator.SetInteger(animationState, (int)EstadosCaractere.andaLeste);
+        }
+        else if(Movement.x < 0){
+            animator.SetInteger(animationState, (int)EstadosCaractere.andaOeste);
+        }
+        else if(Movement.y > 0){
+            animator.SetInteger(animationState, (int)EstadosCaractere.andaNorte);
+        }
+        else if(Movement.y < 0){
+            animator.SetInteger(animationState, (int)EstadosCaractere.andaSul);
+        }
+        else{
+            animator.SetInteger(animationState, (int)EstadosCaractere.idle);
+        }
     }
 }
