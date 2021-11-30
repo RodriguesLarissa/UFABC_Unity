d7b962de43f9f8d7405ecb11169b486f0c398cda
RodriguesLarissa <rodrigues_larissa@outlook.com>
2021-11-30 17:43:33 -0300

    Configuracao da cinemachine na tela principal

---
+++ Projeto_Final/Assets/Scripts/CenaPrincipal/MonoBehaviours/ArredondaPosicao.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+/// <summary>
+/// Classe responsável por corrigir questões relacionadas à imagem renderizada pela Câmera Virtual
+/// </summary>
+public class ArredondaPosicao : CinemachineExtension
+{
+   public float PixelsPerUnit = 32;
+
+    /*
+        Na função PostPipelineStageCallback, analisamos se o personagem está dentro do quadrado,
+        se estiver, capturamos a posição e arredondamos esse valor.
+        Por fim, realizamos a correção através da diferença da posição com o arredondamento dela.
+    */
+    protected override void PostPipelineStageCallback(
+        CinemachineVirtualCameraBase vcam,
+        CinemachineCore.Stage stage, ref CameraState state,
+        float deltaTime)
+    {
+        if(stage == CinemachineCore.Stage.Body){
+            Vector3 pos = state.FinalPosition;
+            Vector3 pos2 = new Vector3(Round(pos.x), Round(pos.y), pos.z);
+            state.PositionCorrection += pos2 - pos;
+        }
+    }
+
+    /*
+        Na função Round, fazemos o arredondamento da posição, multiplicando pelo pixel por unidade
+        e dividindo por ele.
+    */
+    float Round(float x){
+        return Mathf.Round(x * PixelsPerUnit) / PixelsPerUnit;
+    }
+}
